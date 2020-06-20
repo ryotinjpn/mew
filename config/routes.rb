383255eaa8a7c9881d46d1642369d29e66bfb045
ryotinjpn <ryotinjpn@gmail.com>
2020-06-20 20:12:56 +0900

    Add  Main Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'main_pages/home'
+  get 'main_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
