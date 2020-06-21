24e23d8289aa65c5ead4f0d9e3fad52aa7e0fed1
ryotinjpn <ryotinjpn@gmail.com>
2020-06-21 20:56:34 +0900

    add user model test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    # 1. nameとemail、passwordとpassword_confirmationが存在すれば登録できること
+    it "is valid with a name, email, password, password_confirmation" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
+    # 2. nameが空では登録できないこと
+    it "is invalid without a name" do
+      user = build(:user, name: nil)
+      user.valid?
+     expect(user.errors[:name]).to include("を入力してください")
+    end
+
+    # 3. emailが空では登録できないこと
+    it "is invalid without a email" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    # 4. passwordが空では登録できないこと
+    it "is invalid without a password" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    # 5. passwordが存在してもpassword_confirmationが空では登録できないこと
+    it "is invalid without a password_confirmation although with a password" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+
+    # 6. 重複したemailが存在する場合登録できないこと
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
+
+    # 7. passwordが6文字以上であれば登録できること
+    it "is valid with a password that has more than 6 characters " do
+      user = build(:user, password: "123456", password_confirmation: "123456")
+      expect(user).to be_valid
+    end
+
+    # 8. passwordが5文字以下であれば登録できないこと
+    it "is invalid with a password that has less than 5 characters " do
+      user = build(:user, password: "12345", password_confirmation: "12345")
+      user.valid?
+      expect(user.errors[:password]).to include("は6文字以上で入力してください")
+    end
+  end
+end
