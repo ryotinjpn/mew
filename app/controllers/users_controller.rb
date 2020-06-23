cdcb31ee3b26fa63ef292d968b077c898d3eaa33
ryotinjpn <ryotinjpn@gmail.com>
2020-06-23 17:03:15 +0900

    change users controller show action
    ログインユーザーと送信したいユーザーのIDが違うか判別
    違う場合既に部屋があればそこに、なければ新しくroomインスタンスを生成する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!,  only: [:index, :edit, :update, :destroy,:following, :followers, :likes]
+  before_action :authenticate_user!,  only: [:index, :show, :edit, :update, :destroy,:following, :followers, :likes]
   def index
     @users = User.paginate(page: params[:page])
     #@users = User.where(activated: true).paginate(page: params[:page])
@@ -8,6 +8,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.paginate(page: params[:page])
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def following
