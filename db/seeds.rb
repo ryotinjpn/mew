84eb314899bf64325f65067a8bdf531b2c8a8ac8
ryotinjpn <ryotinjpn@gmail.com>
2020-06-26 16:32:47 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 100.times do |n|
-  name  = Faker::Name.name
+  name = Faker::Name.name
+  email = Faker::Internet.email
   email = "example-#{n+1}@co.jp"
   password = "password"
+  profile = Faker::Lorem.sentence 
+  youtube = Faker::Internet.url
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               profile: profile,
+               youtube: youtube
+              )
 end
