0e9a07ef626ccaeca548da2105f2f4848c5c6782
松下諒多 <r.matsushita@nosh.jp>
2021-05-21 20:24:00 +0900

    インデント修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,28 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'main_pages#home'
-  get 'main_pages/protection', to: 'main_pages#protection'
+    devise_for :users
+    root 'main_pages#home'
+    get 'main_pages/protection', to: 'main_pages#protection'
 
-  resources :users do
-    member do
-      get :following, :followers, :likes, :favos
+    resources :users do
+        member do
+            get :following, :followers, :likes, :favos
+        end
+        collection do
+            get 'search'
+        end
     end
-    collection do
-      get 'search'
+
+    resources :posts, only: %i[create show destroy] do
+        resources :comments, only: %i[create destroy]
+        collection do
+            get 'search'
+        end
     end
-  end
 
-  resources :posts, only: %i[create show destroy] do
-    resources :comments, only: %i[create destroy]
-    collection do
-      get 'search'
-    end
-  end
+    resources :relationships, only: %i[create destroy]
+    resources :like_relationships, only: %i[create destroy]
+    resources :favorite_relationships, only: %i[create destroy]
 
-  resources :relationships, only: %i[create destroy]
-  resources :like_relationships, only: %i[create destroy]
-  resources :favorite_relationships, only: %i[create destroy]
-
-  resources :messages, only: [:create]
-  resources :rooms, only: %i[create show index]
+    resources :messages, only: [:create]
+    resources :rooms, only: %i[create show index]
 end
